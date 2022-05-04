8a251abf12eb4970a64f3d5304bf4a9367b51d99
LasseVoss <lfrvo@dtu.dk>
2022-05-04 08:20:00 +0000

    Writing data into functions

--- R/02_clean.R
+++ R/02_clean.R
@@ -7,20 +7,24 @@ library("tidyverse")
 project_data <- read_csv(file = "data/01_project_data.csv")
 
 # Wrangle data ------------------------------------------------------------
-project_data_clean <- project_data %>%
-  filter(log_fold_change > 0 
-         & input.1 > 50 
-         & input.2 > 50 
-         & input.3 > 50) %>%
-  select("barcode", 
-         "sample", 
-         "log_fold_change", 
-         "p", 
-         "HLA", 
-         "Origin", 
-         "Peptide", 
-         "Sequence")
+cleaning_data <- function(project_data){
+  project_data_clean <- project_data %>%
+    filter(log_fold_change > 0 
+           & input.1 > 50 
+           & input.2 > 50 
+           & input.3 > 50) %>%
+    select("barcode", 
+           "sample", 
+           "log_fold_change", 
+           "p", 
+           "HLA", 
+           "Origin", 
+           "Peptide", 
+           "Sequence")
+}
+
+project_data_cleaning <- cleaning_data(project_data)
 
 # Write data --------------------------------------------------------------
-write_csv(x = project_data_clean,
+write_csv(x = project_data_cleaning,
           file = "data/02_project_data_clean.csv")
